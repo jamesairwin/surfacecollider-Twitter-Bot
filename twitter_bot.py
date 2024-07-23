6fb64297e101ee0d59a2537d0e7e5caeb6408e01
jamesairwin <james@jamesirwin.net>
2024-07-23 21:04:12 +0100

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -16,6 +16,17 @@ API_SECRET_KEY = os.getenv('API_SECRET_KEY')
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
 
+api_v1 = tweepy.API(auth)
+
+# Set up Tweepy API v2
+client = tweepy.Client(
+    bearer_token=os.getenv('BEARER_TOKEN'),  # For Tweepy v2, use bearer token
+    consumer_key=API_KEY,
+    consumer_secret=API_SECRET_KEY,
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_TOKEN_SECRET
+)
+
 # MySQL database connection
 db_config = {
     'user': os.getenv('DB_USER'),
